f4d61a71430c30ea408ffc7a452a2068b9e9835d
bhushan <bhushan.dhumal07@gmail.com>
2023-04-23 16:57:55 +0530

    Create seperate login.py and requirement.txt

---
+++ login.py
@@ -0,0 +1,18 @@
+from jproperties import Properties
+import webbrowser
+import urllib.parse
+
+url = "https://api.icicidirect.com/apiuser/login?api_key="
+with open('app.properties', 'r+b') as prop:
+    configs = Properties()
+    configs.load(prop,"utf-8")
+    if len(configs) == 0:
+        print("Error while reading properties files")
+    else :
+        api_key = configs.get("APP_KEY").data if configs.get("APP_KEY") else None
+        webbrowser.open(url+urllib.parse.quote_plus(api_key))
+
+        configs["SESSION_TOKEN"] = input("Enter token value: ")
+        prop.seek(0)
+        prop.truncate(0)
+        configs.store(prop, encoding="utf-8")
